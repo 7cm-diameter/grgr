36d0b1413a6e2470d5d78b808a05c2d7103df6b8
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-08-17 12:32:34 +0900

    Implement helper functions to be used in implementing the `ggplot2` components.

---
+++ grgr/dev/__init__.py
@@ -0,0 +1,43 @@
+""" A module where helper functions for development are implemented """
+from typing import Any, Dict, Iterable, List, Optional
+
+import grgr.dev.typing as tp
+from grgr import _R
+from numpy import ndarray
+from pandas import DataFrame
+
+
+def _is_ndarray(x: Any) -> bool:
+    return isinstance(x, ndarray)
+
+
+def _is_dataframe(x: Any) -> bool:
+    return isinstance(x, DataFrame)
+
+
+def _id_to_alphabet(x: Any) -> str:
+    id_ = str(id(x))
+    return "".join((map(lambda i: chr(ord("@") + int(i) + 1), str(id_))))
+
+
+def _format_as_arg(k: str, v: Any, ignored: List[str]) -> Optional[str]:
+    if v is None or k in ignored:
+        return None
+    if _is_ndarray(v) or _is_dataframe(v):
+        varname = _id_to_alphabet(v)
+        _R.assign(varname, v)
+        return f"{k}={varname}"
+    return f"{k}={v}"
+
+
+def _filter_none(x: Iterable[Optional[Any]]) -> filter:
+    return filter(lambda x_: x_ is not None, x)
+
+
+def join_as_rargs(d: Dict, ignored: List[str] = []) -> tp.RCode:
+    # following three variables are ignored by defalut
+    ignored.extend(["ignores", "self", "args", "kwargs"])
+    _fargs: map[Optional[str]] = map(
+        lambda kv: _format_as_arg(kv[0], kv[1], ignored), d.items())
+    fargs = list(_filter_none(_fargs))
+    return ",".join(fargs)
