2a6c240c16cb0074c4e1601ca37d9068f60da350
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-08-18 17:36:08 +0900

    Implement Scales that control the apprerance of plot

---
+++ grgr/ggplot2/scale.py
@@ -0,0 +1,28 @@
+from copy import copy
+
+import grgr.dev.typing as tp
+from grgr.dev import _filter_none, dict_to_rargs, iter_to_rargs
+
+
+class Appearance(tp.Join):
+    """
+    Appearance is a class that control the plot apperance.
+    """
+    def __init__(self, name: str, *args, **kwargs):
+        self._s = str()
+        reqargs = dict_to_rargs(locals(), ["name"])
+        posargs = iter_to_rargs(args)
+        kwargs = dict_to_rargs(kwargs, ["name"])
+        rargs = ",".join(_filter_none([reqargs, posargs, kwargs]))
+        self._s = f"{name}({rargs})"
+
+    def __repr__(self) -> str:
+        return self._s
+
+    def __add__(self, other: tp.Join) -> "Appearance":
+        clone = copy(self)
+        clone._s += f" + \n  {other.tor()}"
+        return clone
+
+    def tor(self) -> tp.RCode:
+        return self._s
