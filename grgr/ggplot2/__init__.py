7fa766edd9f075dfd54c66d04aad19e231223750
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-08-18 17:36:29 +0900

    Implement Scales that control the apprerance of plot

--- grgr/ggplot2/__init__.py
+++ grgr/ggplot2/__init__.py
@@ -1,8 +1,11 @@
-from typing import Optional, Union
+from typing import Optional, Tuple, Union
 
+from grgr.dev.typing import T, U
 from grgr.ggplot2.basic import Aesthetic, GGPlot
 from grgr.ggplot2.layer import Layer
-from numpy import ndarray
+from grgr.ggplot2.scale import Appearance
+from numpy import array, ndarray, str_
+from numpy.typing import NDArray
 from pandas import DataFrame
 
 
@@ -96,3 +99,96 @@ def geom_violin(data: Optional[DataFrame] = None,
                 mapping: Optional[Aesthetic] = None,
                 **kwargs):
     return Layer("geom_violin", data, mapping, **kwargs)
+
+
+# Scales
+def labs(title: Optional[str] = None,
+         subtitle: Optional[str] = None,
+         caption: Optional[str] = None,
+         tag: Optional[str] = None,
+         alt: Optional[str] = None,
+         alt_insight: Optional[str] = None,
+         **kwargs) -> Appearance:
+    return Appearance("labs",
+                      title=title,
+                      subtitle=subtitle,
+                      caption=caption,
+                      tag=tag,
+                      alt=alt,
+                      alt_insight=alt_insight,
+                      **kwargs)
+
+
+def xlab(label: str) -> Appearance:
+    return Appearance("xlab", label=label)
+
+
+def ylab(label: str) -> Appearance:
+    return Appearance("xlab", label=label)
+
+
+def ggtitle(label, subtitle: Optional[str] = None) -> Appearance:
+    return Appearance("ggtitle", label=label, subtitle=subtitle)
+
+
+def lims(x: Optional[Tuple[T, T]], y: Optional[Tuple[U, U]]) -> Appearance:
+    return Appearance("lims", x=array(x), y=array(y))
+
+
+# TODO: Use `xlim` and `ylim` instead of `lims`.
+def xlim(x: Tuple[T, T]) -> Appearance:
+    return Appearance("lims", x=array(x))
+
+
+def ylim(y: Tuple[T, T]) -> Appearance:
+    return Appearance("lims", y=array(y))
+
+
+def scale_color_continuous(colorscale: str = '"gradient"') -> Appearance:
+    return Appearance("scale_color_continuous", type=colorscale)
+
+
+def scale_fill_continuous(colorscale: str = '"gradient"') -> Appearance:
+    return Appearance("scale_fill_continuous", type=colorscale)
+
+
+def scale_color_discrete(colorscale: str = '"gradient"') -> Appearance:
+    return Appearance("scale_color_discrete", type=colorscale)
+
+
+def scale_fill_discrete(colorscale: str = '"gradient"') -> Appearance:
+    return Appearance("scale_fill_discrete", type=colorscale)
+
+
+def scale_color_gradient(low: str, high: str, **kwargs) -> Appearance:
+    return Appearance("scale_color_gradient", low=low, high=high, **kwargs)
+
+
+def scale_fill_gradient(low: str, high: str, **kwargs) -> Appearance:
+    return Appearance("scale_fill_gradient", low=low, high=high, **kwargs)
+
+
+def scale_color_gradient2(low: str, mid: str, high: str,
+                          **kwargs) -> Appearance:
+    return Appearance("scale_color_gradient2",
+                      low=low,
+                      mid=mid,
+                      high=high,
+                      **kwargs)
+
+
+def scale_fill_gradient2(low: str, mid: str, high: str,
+                         **kwargs) -> Appearance:
+    return Appearance("scale_fill_gradient2",
+                      low=low,
+                      mid=mid,
+                      high=high,
+                      **kwargs)
+
+
+def scale_color_gradientn(colors: NDArray[str_], **kwargs) -> Appearance:
+    return Appearance("scale_color_gradientn", colors=colors, **kwargs)
+
+
+def scale_fill_gradientn(colors: NDArray[str_], **kwargs) -> Appearance:
+    return Appearance("scale_fill_gradientn", colors=colors, **kwargs)
