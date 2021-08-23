8f1150538dffe37737538aad6c8946c51584aaea
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-08-23 11:14:03 +0900

    Add a test for `scale.py`

---
+++ tests/test_scale.py
@@ -0,0 +1,21 @@
+from typing import Any, Dict, Tuple
+
+import grgr.dev as d
+import pytest
+from grgr.ggplot2.scale import Appearance
+from numpy import array
+
+A_ = array(['"foo"', '"bar"'])
+
+
+@pytest.mark.parametrize("args, kwargs, answer", [
+    (('"foo"', 2), {}, 'test("foo",2)'),
+    ((tuple(A_)), {}, f"test({d._id_to_alphabet(A_)})"),
+    ((), {
+        "foo": 2,
+        "bar": A_
+    }, f"test(foo=2,bar={d._id_to_alphabet(A_)})"),
+    ((), {}, "test()"),
+])
+def test_appearance(args: Tuple[Any], kwargs: Dict[str, Any], answer: str):
+    print(Appearance("test", *args, **kwargs).tor())
