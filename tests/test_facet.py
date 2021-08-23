63c3903748b3aaf089cee2f8e2aa54eaade8eab6
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-08-23 12:17:28 +0900

    Add test for `facet.py`

---
+++ tests/test_facet.py
@@ -0,0 +1,21 @@
+from typing import Any, Dict, Tuple
+
+import grgr.dev as d
+import pytest
+from grgr.ggplot2.facet import Facet
+from numpy import array
+
+A_ = array(['"foo"', '"bar"'])
+
+
+@pytest.mark.parametrize("args, kwargs, answer", [
+    (('"foo"', 2), {}, 'test("foo",2)'),
+    ((A_, '"foo"'), {}, f'test({d._id_to_alphabet(A_)},"foo")'),
+    ((), {
+        "foo": 2,
+        "bar": A_
+    }, f"test(foo=2,bar={d._id_to_alphabet(A_)})"),
+    ((), {}, "test()"),
+])
+def test_appearance(args: Tuple[Any], kwargs: Dict[str, Any], answer: str):
+    assert Facet("test", *args, **kwargs).tor() == answer
